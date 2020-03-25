f9c293f36542a685e8fe5976758697aa0c2175c9
EmCharlotta <em13charlotte@gmail.com>
2020-03-25 21:30:37 +0300

    Homework Lesson 250320

---
+++ src/test250320/Cow.java
@@ -0,0 +1,14 @@
+package test250320;
+
+public class Cow extends FarmAnimals implements GiveResources {
+
+    @Override
+    public void canBeEaten() {
+
+    }
+
+    @Override
+    public void giveResources() {
+
+    }
+}
